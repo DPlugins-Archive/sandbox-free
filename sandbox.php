00f7d0bce7223ebcfb7051cd750dff58e3b4a371
oxyrealm-dev <hello@oxyrealm.com>
2021-03-11 22:14:56 +0800

    🌱

---
+++ sandbox.php
@@ -0,0 +1,255 @@
+<?php
+/**
+ * Sandbox
+ *
+ * @wordpress-plugin
+ * Plugin Name:         Sandbox
+ * Description:         Sandbox for Oxygen Builder.
+ * Version:             0.0.1
+ * Author:              oxyrealm
+ * Author URI:          https://oxyrealm.com
+ * Requires at least:   5.6
+ * Tested up to:        5.7
+ * Requires PHP:        7.4
+ * Text Domain:         sandbox
+ * Domain Path:         /languages
+ *
+ * @package             Sandbox
+ * @author              oxyrealm <hello@oxyrealm.com>
+ * @link                https://oxyrealm.com
+ * @since               0.0.1
+ * @copyright           2021 oxyrealm
+ * @version             0.0.1
+ */
+
+namespace Oxyrealm\Modules\Sandbox;
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'OXYREALM_SANDBOX_VERSION', '0.0.1' );
+define( 'OXYREALM_SANDBOX_DB_VERSION', '001' );
+
+define( 'OXYREALM_SANDBOX_FILE', __FILE__ );
+define( 'OXYREALM_SANDBOX_PATH', dirname( OXYREALM_SANDBOX_FILE ) );
+define( 'OXYREALM_SANDBOX_MIGRATION_PATH', OXYREALM_SANDBOX_PATH . '/database/migrations/' );
+define( 'OXYREALM_SANDBOX_URL', plugins_url( '', OXYREALM_SANDBOX_FILE ) );
+define( 'OXYREALM_SANDBOX_ASSETS', OXYREALM_SANDBOX_URL . '/public' );
+
+use Oxyrealm\Aether\Assets;
+use Oxyrealm\Aether\Utils\DB;
+use Oxyrealm\Aether\Utils\Migration;
+use WP_Admin_Bar;
+
+final class Sandbox {
+
+    public string $module_id = 'aether_m_sandbox';
+
+    protected bool $active = false;
+
+    protected array $post_metas = [
+        'ct_builder_shortcodes',
+        'ct_other_template',
+        'ct_page_settings',
+        'ct_parent_template',
+        'ct_render_post_using',
+        'ct_use_inner_content',
+    ];
+
+    protected array $options = [
+        'ct_components_classes',
+        'ct_custom_selectors',
+        'ct_global_settings',
+        'ct_style_folders',
+        'ct_style_sets',
+        'ct_style_sheets',
+        'ct_svg_sets',
+        'oxygen_vsb_comments_list_templates',
+        'oxygen_vsb_easy_posts_templates',
+        'oxygen_vsb_element_presets',
+        'oxygen_vsb_global_colors',
+        'oxygen_vsb_google_fonts_cache',
+        'oxygen_vsb_latest_typekit_fonts',
+        'oxygen_vsb_universal_css_cache',
+    ];
+
+    protected $secret;
+
+	public function __construct() {
+		register_activation_hook( AETHER_FILE, [ $this, 'plugin_activate' ] );
+		register_deactivation_hook( AETHER_FILE, [ $this, 'plugin_deactivate' ] );
+
+		add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
+	}
+
+    public function plugin_activate(): void {
+		if ( ! get_option( 'oxyrealm_sandbox_installed' ) ) {
+			update_option( 'oxyrealm_sandbox_installed', time() );
+		}
+
+		$installed_db_version = get_option( 'oxyrealm_sandbox_db_version' );
+        
+		if ( ! $installed_db_version || intval( $installed_db_version ) !== intval( OXYREALM_SANDBOX_DB_VERSION ) ) {
+			Migration::migrate( OXYREALM_SANDBOX_MIGRATION_PATH, "\\Oxyrealm\\Modules\\Sandbox\\Database\\Migrations\\", $installed_db_version ?: 0, OXYREALM_SANDBOX_DB_VERSION );
+			update_option( 'oxyrealm_sandbox_db_version', OXYREALM_SANDBOX_DB_VERSION );
+		}
+
+		update_option( 'oxyrealm_sandbox_version', OXYREALM_SANDBOX_VERSION );
+
+        $this->set_secret();
+	}
+
+	public function plugin_deactivate(): void {
+        $this->unset_secret();
+	}
+
+    private function is_active(): bool {
+        if ( current_user_can( 'manage_options' ) || $this->validate_cookie() ) {
+            return true;
+        }
+
+        $secret = $_GET[$this->module_id] ?? false;
+        if ( $secret ) {
+            if ( $secret === $this->secret ) {
+                $this->set_cookie();
+            } else {
+                $this->unset_cookie();
+            }
+        }
+        
+        return false;
+    }
+
+    private function validate_cookie(): bool {
+        $cookie = $_COOKIE[$this->module_id] ?? false;
+        return $cookie && $cookie === $this->secret;
+    }
+
+    private function set_cookie(): void {
+        setcookie($this->module_id, $this->secret, time() + DAY_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN);
+        
+        if ( isset( $_SERVER['REQUEST_URI'] ) && wp_redirect( $_SERVER['REQUEST_URI'] ) ) {
+            exit;
+        }
+    }
+
+    private function unset_cookie(): void {
+        setcookie($this->module_id, null, -1, COOKIEPATH, COOKIE_DOMAIN);
+        
+        if ( isset( $_SERVER['REQUEST_URI'] ) && wp_redirect( $_SERVER['REQUEST_URI'] ) ) {
+            exit;
+        }
+    }
+
+    public function set_secret(): void {
+        update_option( "{$this->module_id}_secret", wp_generate_uuid4() );
+    }
+
+    public function unset_secret(): void {
+        delete_option( "{$this->module_id}_secret" );
+    }
+
+    public static function run() {
+        static $instance = false;
+
+		if ( ! $instance ) {
+			$instance = new Sandbox();
+		}
+
+		return $instance;
+    }
+
+    public function init_plugin() {
+        Assets::register_style("{$this->module_id}-admin", AETHER_URL . '/modules/sandbox/assets/css/admin.css');
+
+        $this->secret = get_option( "{$this->module_id}_secret" );
+        $this->active = $this->is_active();
+
+		add_action( 'init', [ $this, 'boot' ] );
+    }
+
+    public function boot() {
+        Assets::do_register();
+        
+        if ( ! $this->active ) {
+            return;
+        }
+
+        wp_enqueue_style( "{$this->module_id}-admin" );
+
+        foreach ( array_keys( wp_load_alloptions() ) as $option ) {
+            if ( strpos( $option, "oxygen_vsb_" ) === 0 ) {
+                add_filter( "pre_option_{$option}", [ $this, 'pre_get_option' ], 0, 3 );
+                add_filter( "pre_update_option_{$option}", [ $this, 'pre_update_option' ], 0, 3 );
+            }
+        }
+
+        // foreach ( $this->options as $option ) {
+        //     add_filter( "pre_option_{$option}", [ $this, 'pre_get_option' ], 0, 3 );
+        //     add_filter( "pre_update_option_{$option}", [ $this, 'pre_update_option' ], 0, 3 );
+        // }
+
+        add_filter( 'get_post_metadata', [ $this, 'get_post_metadata' ], 0, 4 );
+        add_filter( 'update_post_metadata', [ $this, 'update_post_metadata' ], 0, 5 );
+        add_filter( 'delete_post_metadata', [ $this, 'delete_post_metadata' ], 0, 5 );
+
+        add_action( 'admin_bar_menu', [ $this, 'admin_bar_node' ], 100 );
+    }
+
+    public function pre_get_option( $pre_option, string $option, $default ) {
+        if ( $option === 'oxygen_vsb_universal_css_cache' ) {
+            return 'false';
+        }
+        
+        if ( DB::has( 'options', [ 'option_name' => "{$this->module_id}_{$option}", ] ) ) {
+            $pre_option = get_option( "{$this->module_id}_{$option}", $default );
+        }
+
+        return $pre_option;
+    }
+
+    public function pre_update_option( $value, $old_value, string $option ) {
+        if ( $option === 'oxygen_vsb_universal_css_cache' ) {
+            return $old_value;
+        }
+
+        update_option( "{$this->module_id}_{$option}", $value );
+        return $old_value;
+    }
+
+    public function update_post_metadata( $check, $object_id, $meta_key, $meta_value, $prev_value ) {
+        return in_array( $meta_key, $this->post_metas ) 
+            ? update_metadata( 'post', $object_id, "{$this->module_id}_{$meta_key}", $meta_value, $prev_value ) 
+            : $check;
+    }
+
+    public function delete_post_metadata( $delete, $object_id, $meta_key, $meta_value, $delete_all ) {
+        return in_array( $meta_key, $this->post_metas )
+            ? delete_metadata( 'post', $object_id, "{$this->module_id}_{$meta_key}", $meta_value, $delete_all )
+            : $delete;
+    }
+
+    public function get_post_metadata( $value, $object_id, $meta_key, $single ) {
+        if ( in_array( $meta_key, $this->post_metas ) && metadata_exists( 'post', $object_id, "{$this->module_id}_{$meta_key}" ) ) {
+            $value = get_metadata( 'post', $object_id, "{$this->module_id}_{$meta_key}", $single );
+            if ( $single && is_array( $value ) ) {
+                $value = [$value];
+            }
+        }
+        return $value;
+    }
+
+    public function admin_bar_node( WP_Admin_Bar $wp_admin_bar ) {
+        $wp_admin_bar->add_node([
+            'id'    => 'sandbox',
+            'title' => 'Sandbox <span class="text-green-400">●</span>',
+            'meta' => [
+                'title' => 'Sandbox Mode - Aether'
+            ]
+        ]);
+    }
+
+}
+
+if ( class_exists( '\Aether' ) ) {
+    $aether->container['modules']['sandbox'] = Sandbox::run();
+}
